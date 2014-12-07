4754ee81a78b44e5f268d39bef637feab90612f6
ShuzoN <d.namu.r21@gmail.com>
2014-12-07 12:09:13 +0900

    add dotfilesLink.sh

---
+++ dotfilesLink.sh
@@ -0,0 +1,9 @@
+
+# dotfile のシンボリックリンクを春スクリプト
+
+ln -sf ~/dotfiles/.vimrc ~/.vimrc
+ln -sf ~/dotfiles/.zshrc ~/.zshrc
+ln -sf ~/dotfiles/.bash_profile ~/.bash_profile
+ln -sf ~/dotfiles/.vimperatorrc ~/.vimperatorrc
+
+
