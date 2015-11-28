3cca144108ed2e34641348b64a88f8691a06e12a
ShuzoN <d.namu.r21@gmail.com>
2015-11-28 17:22:50 +0900

    proxy_off.sh no proxyの場合, macのネットワーク設定を切り替えるコマンドを追加

--- bin/proxy_off.sh
+++ bin/proxy_off.sh
@@ -5,6 +5,10 @@
 #
 # --- abst ---
 # nut proxy off
+# macのネットワーク設定をoffにする.
+
+# sudoコマンドをpasswdなしで使う方法
+# http://qiita.com/pugiemonn/items/baaf7243ded2e6ab94f9
 #************************************************************
 unset http_proxy
 unset HTTP_PROXY
@@ -20,3 +24,5 @@ echo "NUT proxy disable"
 #heck file exist -e
 [ -e ~/.wgetrc ] && unlink ~/.wgetrc && echo "wgetrc is deleted"
 [ -e ~/.curlrc ] && unlink ~/.curlrc && echo "curlrc is deleted"
+network_location="home" #macのguiネットワーク環境名
+sudo networksetup -switchtolocation $network_location > /dev/null
