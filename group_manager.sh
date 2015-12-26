e3970e2004250037a89af8eace48feb3f29f486c
ShuzoN <d.namu.r21@gmail.com>
2015-12-26 19:01:27 +0900

    group_manager周りの環境変数を設定するファイルを追加

---
+++ group_manager.sh
@@ -0,0 +1,23 @@
+# export SMTP_ADRESS=smtp.gmail.com
+# export SMTP_PORT=587
+# export EMAIL_DOMAIN=gmail.com
+# export SMTP_AUTH=plain
+# export SMTP_TLS=false
+# export EMAIL_USERNAME=d.namu.r21@gmail.com
+# export EMAIL_BCC='d.namu.r21@gmail.com'
+# export EMAIL_PASSWORD=tpuslkxjcfhcnjwv # gmailの場合は2段階認証を設定後, アプリ固有のパスワードを設定する
+# export EMAIL_SENDER='Shuzo Nakamura <d.namu.r21@gmail.com>'
+# export DEFAULT_URL=https://nutfes-group-manager.herokuapp.com
+# ---------
+export SMTP_ADRESS=stn.nagaokaut.ac.jp
+export SMTP_PORT=465
+export EMAIL_DOMAIN=stn.nagaokaut.ac.jp
+export SMTP_AUTH=plain
+export SMTP_TLS=true
+export EMAIL_USERNAME='s133367'
+export EMAIL_BCC='d.namu.r21@gmail.com'
+export EMAIL_PASSWORD='Mu40Tsu3' # gmailの場合は2段階認証を設定後, アプリ固有のパスワードを設定する
+export EMAIL_SENDER='Shuzo Nakamura <d.namu.r21@gmail.com>'
+export DEFAULT_URL=https://nutfes-group-manager.herokuapp.com
+
+. $HOME/dotfiles/group_manager_pw.sh
