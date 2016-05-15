281d1dffda4c3c5551178abc748d0957a586cd51
ShuzoN <d.namu.r21@gmail.com>
2016-05-15 16:27:09 +0900

    GroupManagerのpwをignore

--- group_manager.sh
+++ group_manager.sh
@@ -14,12 +14,9 @@ export SMTP_PORT=465
 export EMAIL_DOMAIN=stn.nagaokaut.ac.jp
 export SMTP_AUTH=plain
 export SMTP_TLS=true
-export EMAIL_USERNAME='s133367'
-export EMAIL_BCC='d.namu.r21@gmail.com'
-export EMAIL_PASSWORD='Mu40Tsu3' # gmailの場合は2段階認証を設定後, アプリ固有のパスワードを設定する
-export EMAIL_SENDER='Shuzo Nakamura <d.namu.r21@gmail.com>'
 export DEFAULT_URL=https://nutfes-group-manager.herokuapp.com
 
+. $HOME/dotfiles/group_manager_email.sh
 . $HOME/dotfiles/group_manager_pw.sh
 export RAILS_SERVE_STATIC_FILES=true
 
